3dc918f7d65c2f690c554121a9e6baed4e9ebd24
aem <111235272+aemiio@users.noreply.github.com>
2024-11-26 01:30:54 +0800

    feat: Implement ShoppingListItem component and refactor App layout

--- App.tsx
+++ App.tsx
@@ -1,67 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View, Alert } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { theme } from "./theme";
+import { ShoppingListItem } from "./components/ShoppingListItem";
 
 export default function App() {
-  const handleDelete = () => {
-    Alert.alert(
-      "Are you sure you want to delete this?",
-      "It will be gone for good",
-      [
-        {
-          text: "Yes",
-          onPress: () => console.log("Deleting..."),
-          style: "destructive",
-        },
-        { text: "Cancel", style: "cancel" },
-      ]
-    );
-  };
   return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <StatusBar style="auto" />
-        <Text style={styles.itemText}>Coffee</Text>
-        <TouchableOpacity
-          onPress={handleDelete}
-          style={styles.button}
-          activeOpacity={0.8}
-        >
-          <Text style={styles.buttonText}>Delete</Text>
-        </TouchableOpacity>
-      </View>
+    <View style={styles.containter}>
+      <ShoppingListItem name="Coffee" />
+      <ShoppingListItem name="Tea" />
+      <ShoppingListItem name="Milk" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  itemContainer: {
-    paddingVertical: 16,
-    paddingHorizontal: 8,
-    borderBottomColor: theme.colorCerulean,
-    borderBottomWidth: 1,
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  container: {
-    flex: 1,
+  containter: {
+    flex: 1, // Makes the container take the full screen height
     backgroundColor: theme.colorWhite,
+    flexDirection: "column",
     justifyContent: "center",
-  },
-  itemText: {
-    fontSize: 30,
-    fontWeight: "regular",
-  },
-  button: {
-    backgroundColor: theme.colorBlack,
-    padding: 8,
-    borderRadius: 6,
-  },
-  buttonText: {
-    color: theme.colorWhite,
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    letterSpacing: 1,
+    paddingHorizontal: 16,
+    marginVertical: 18
   },
 });
