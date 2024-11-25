e7e0b72a1f4ac82b1badc3b2dccc1e1c15260470
aem <111235272+aemiio@users.noreply.github.com>
2024-11-25 23:34:54 +0800

    Refactor App component and add theme support

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <StatusBar style="auto" />
+        <Text style={styles.itemText}>Coffee</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  itemContainer: {
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+    borderBottomColor: theme.colorCerulean,
+    borderBottomWidth: 1,
+    alignItems: "center",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.colorWhite,
+    justifyContent: "center",
+  },
+  itemText: {
+    fontSize: 30,
+    fontWeight: "regular",
   },
 });
