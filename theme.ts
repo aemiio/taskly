f5d5b765885e15d5f25b993afada8496ebf728c5
aemiio <cjyramae@gmail.com>
2024-12-29 21:45:13 +0800

    refactor: Remove unused files and components to clean up the project

--- theme.ts
+++
@@ -1,8 +0,0 @@
-export const theme = {
-    colorCerulean: "#1a759f",
-    colorWhite: "#fff",
-    colorBlack: "#000",
-    colorGrey: "grey",
-    colorLightGrey: "#eee",
-    colorRed: "#ee6055"
-}
