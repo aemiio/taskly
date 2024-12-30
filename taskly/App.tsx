a4f43a3277a1a67f33e3a589313dde21adc90ab6
aemiio <cjyramae@gmail.com>
2024-12-30 20:48:07 +0800

    feat: Add theme configuration, ESLint setup, and ListItem component with delete functionality

--- taskly/App.tsx
+++ taskly/App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View} from "react-native";
+import { theme } from "./theme";
+import {ListItem} from "./components/screen1"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ListItem name="coffee" isCompleted={false}></ListItem>
+      <ListItem name="milk"  isCompleted></ListItem>
+      <ListItem name="tea" isCompleted></ListItem>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.colorWhite,
+    justifyContent: "center",
   },
 });
