e40c48a22a1eb78a9a226180c51eb0f4d1857099
aemiio <cjyramae@gmail.com>
2024-12-30 23:37:28 +0800

    feat: Add navigation links and new screens for Counter and Idea

--- taskly/app/index.tsx
+++ taskly/app/index.tsx
@@ -1,10 +1,17 @@
 import { StyleSheet, View } from "react-native";
 import { theme } from "../theme";
 import { ListItem } from "../components/screen1";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href="/counter"
+        style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
+      >
+        Go to Counter
+      </Link>
       <ListItem name="coffee" isCompleted={false}></ListItem>
       <ListItem name="milk" isCompleted></ListItem>
       <ListItem name="tea" isCompleted></ListItem>
